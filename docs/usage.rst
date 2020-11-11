41ca4be7e5c47bd8929ec70d89c904c5e3a25426
Daryl Kow <dk3154@columbia.edu>
2020-11-11 21:53:47 +0800

    first commit

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use cipher_dk3154 in a project::
+
+    import cipher_dk3154
---
+++ tests/test_cipher_dk3154.py
@@ -0,0 +1,5 @@
+from cipher_dk3154 import __version__
+from cipher_dk3154 import cipher_dk3154
+
+def test_version():
+    assert __version__ == '0.1.0'
